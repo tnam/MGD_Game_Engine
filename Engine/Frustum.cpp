97b25fd1d9ae37ed467349e4a8fc2245b66c1b57
DT Nam Cam <dnam86@gmail.com>
2015-09-27 00:23:55 +0200

    Optimize frustum planes computation

--- Engine/Frustum.cpp
+++ Engine/Frustum.cpp
@@ -7,8 +7,6 @@ void Plane::BuildPlane(const glm::vec3& point, const glm::vec3& normal)
 }
 
 Frustum::Frustum() 
-	: m_NearTopLeft(0), m_NearTopRight(0), m_NearBottomLeft(0), m_NearBottomRight(0)
-	, m_FarTopLeft(0), m_FarTopRight(0), m_FarBottomLeft(0), m_FarBottomRight(0)
 {
 }
 
@@ -29,17 +27,6 @@ void Frustum::BuildPlanes(const glm::vec3& position, const glm::vec3& forward, c
 {
 	glm::vec3 nearCenter = position + (forward * m_NearDistance);
 	glm::vec3 farCenter = position + (forward * m_FarDistance);
-
-	// Define the 8 frustum's corners
-	m_NearTopLeft = nearCenter - right * m_NearWidth + up * m_NearHeight;
-	m_NearBottomLeft = nearCenter - right * m_NearWidth - up * m_NearHeight;
-	m_NearTopRight = nearCenter + right * m_NearWidth + up * m_NearHeight;
-	m_NearBottomRight = nearCenter + right * m_NearWidth - up * m_NearHeight;
-
-	m_FarTopLeft = farCenter - right * m_FarWidth + up * m_FarHeight;
-	m_FarBottomLeft = farCenter - right * m_FarWidth - up * m_FarHeight;
-	m_FarTopRight = farCenter + right * m_FarWidth + up * m_FarHeight;
-	m_FarBottomRight = farCenter + right * m_FarWidth - up * m_FarHeight;
 
 	// Build Near plane
 	m_Planes[static_cast<int>(EPlane::NEAR)].BuildPlane(nearCenter, forward);
